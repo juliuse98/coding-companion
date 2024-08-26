243698941747821633b5430414fa4c134155ded4
juliuse98 <julius.ewert1@gmail.com>
2024-08-26 23:16:09 +0200

    Add some basic classes from opengl renderer project

---
+++ include/vertex_array.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "VertexBuffer.h"
+namespace cabbage {
+class VertexBufferLayout;
+
+	class VertexArray
+	{
+	private:
+	public:
+		unsigned int m_RendererId;
+		unsigned int m_lastAttribArrayId;
+		VertexArray();
+		~VertexArray();
+		void AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout);
+
+		void Bind() const;
+		void Unbind() const;
+	};
+}
+
