9a5cdaacaa812fdf38f50bbf09cac94cc8f0ea20
juliuse98 <julius.ewert1@gmail.com>
2024-09-15 22:30:30 +0200

    Add Temporary Fix for ZombieThread See #11

--- src/main.cpp
+++ src/main.cpp
@@ -94,74 +94,40 @@ void makeWindowTransparentAndClickThrough(GLFWwindow* window)
 }
 #elif defined(_WIN32)
 
-bool shouldCLose = false;
-
 WNDPROC originalProc = nullptr;
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    // LOG(WARNING) << uMsg << ", " << wParam;
     switch (uMsg)
     {
     case WM_SYSCOMMAND:
         if ((wParam & 0xfff0) == SC_MINIMIZE)
         {
             // Ignore minimize requests
+            LOG(ERROR) << "MINIMIZE";
             return 0; // Block minimize
         }
         if ((wParam & 0xfff0) == SC_RESTORE)
         {
             // Ignore restore requests
+            LOG(ERROR) << "RESTORE";
             return 0; // Block restore
         }
-        break;
     }
     return CallWindowProc(originalProc, hwnd, uMsg, wParam, lParam);
     // Handle other cases or pass them to the default window procedure
-}
-void ReapplyWindowStyles(HWND hwnd)
-{
-    LONG_PTR style = GetWindowLongPtr(hwnd, GWL_EXSTYLE);
-    SetWindowLongPtr(hwnd, GWL_EXSTYLE, style | WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOPMOST | WS_EX_TOOLWINDOW);
-    SetLayeredWindowAttributes(hwnd, RGB(0, 0, 0), 0xFF, LWA_ALPHA);
-    SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
-}
-
-void SetTimerForStyles(HWND hwnd)
-{
-    SetTimer(hwnd, 1, 1000, [](HWND hwnd, UINT msg, UINT_PTR idEvent, DWORD dwTime) { ReapplyWindowStyles(hwnd); });
 }
 
 void makeWindowTransparentAndClickThrough(GLFWwindow* window)
 {
     HWND hwnd = glfwGetWin32Window(window);
 
-    // Apply initial window styles
-    ReapplyWindowStyles(hwnd);
 
     originalProc = (WNDPROC)GetWindowLongPtr(hwnd, GWLP_WNDPROC);
-    //  Set a timer to reapply styles periodically
-    //  SetTimerForStyles(hwnd);
-
     // Hook the window procedure to intercept messages
     SetWindowLongPtr(hwnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(WindowProc));
 }
 #endif
-
-void updateUV(cabbage::UVCoordinate vertices[], cabbage::SpriteUVRect rect)
-{
-    vertices[0].u = rect.u;
-    vertices[0].v = rect.v + rect.height;
-
-    vertices[1].u = rect.u + rect.width;
-    vertices[1].v = rect.v + rect.height;
-
-    vertices[2].u = rect.u;
-    vertices[2].v = rect.v;
-
-    vertices[3].u = rect.u + rect.width;
-    vertices[3].v = rect.v;
-}
 
 int main(int argc, char* argv[])
 {
@@ -169,7 +135,6 @@ int main(int argc, char* argv[])
     el::Configurations conf("./easyloggingpp.conf");
     el::Loggers::reconfigureLogger("default", conf);
     el::Loggers::reconfigureAllLoggers(conf);
-
     LOG(INFO) << "Start Init";
     if (!glfwInit())
     {
@@ -189,6 +154,7 @@ int main(int argc, char* argv[])
     cwindow.setWindowHint(GLFW_FLOATING, GLFW_TRUE);
     cwindow.setWindowHint(GLFW_FOCUSED, GLFW_FALSE);
     cwindow.setWindowHint(GLFW_FOCUS_ON_SHOW, GLFW_FALSE);
+    cwindow.setWindowHint(GLFW_MOUSE_PASSTHROUGH, GLFW_TRUE);
     std::string windowTitle = "Coding Companion";
     if (!cwindow.init(mode->width - 1, mode->height - 1, windowTitle))
     {
@@ -216,79 +182,84 @@ int main(int argc, char* argv[])
     LOG(INFO) << "Init GLEW Complete";
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
+    coco::CCObject rootObj;
     coco::CCObject obj;
     coco::CCObject child1;
-
-    cabbage::SpriteRenderer        r;
-    std::vector<cabbage::Texture*> textures;
-
-    stbi_set_flip_vertically_on_load(true);
-
-    LOG(INFO) << "Init GLEW Complete";
-    cabbage::Texture* texture1 = cabbage::TextureLoader::load("resources/textures/arrowsRed.png");
-    textures.push_back(texture1);
-    cabbage::Texture* texture2 = cabbage::TextureLoader::load("resources/textures/arrowsGreen.png");
-    textures.push_back(texture2);
-
-    cabbage::Sprite      catSprite(textures[0]);
-    cabbage::Sprite      arrowsGreenSprite(textures[1]);
-    cabbage::SpriteSheet spriteSheet = cabbage::SpriteSheet(textures.at(0));
-    spriteSheet.addSpriteUVRect(0.0f, 0.66f, 0.33f, 0.33f);
-    spriteSheet.addSpriteUVRect(0.33f, 0.66f, 0.33f, 0.33f);
-    spriteSheet.addSpriteUVRect(0.66f, 0.66f, 0.33f, 0.33f);
-    spriteSheet.addSpriteUVRect(0.0f, 0.33f, 0.33f, 0.33f);
-    spriteSheet.addSpriteUVRect(0.33f, 0.33f, 0.33f, 0.33f);
-    spriteSheet.addSpriteUVRect(0.66f, 0.33f, 0.33f, 0.33f);
-    spriteSheet.addSpriteUVRect(0.0f, 0.0f, 0.33f, 0.33f);
-    spriteSheet.addSpriteUVRect(0.33f, 0.0f, 0.33f, 0.33f);
-    obj.SetSprite(&catSprite);
-    obj.GetSprite()->SetUVRect(spriteSheet.getSpriteUVRect(0));
-    child1.SetSprite(&arrowsGreenSprite);
-    child1.GetTransform().SetPosition(glm::vec3(0, 100, 0));
+    rootObj.addChild(&obj);
     obj.addChild(&child1);
-    r.prepareDraw(&obj);
-
-    LOG(INFO) << "Init GLEW Complete";
-
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    const int                       target_fps = 60;
-    const std::chrono::milliseconds target_frame_duration(1000 / target_fps);
-    auto                            lastTime = std::chrono::high_resolution_clock::now();
-
-    int spriteId = 0;
-    while (!glfwWindowShouldClose(cwindow.GetGLFWwindow()) && !shouldCLose)
     {
-        auto frame_start = std::chrono::high_resolution_clock::now();
-        glClear(GL_COLOR_BUFFER_BIT);
-        glPolygonMode(GL_FRONT, GL_FILL);
-        glPolygonMode(GL_BACK, GL_LINE);
 
-        obj.GetSprite()->SetUVRect(spriteSheet.getSpriteUVRect(spriteId));
-        obj.GetTransform().SetPosition(glm::vec3(100.0f * spriteId, 100.0f, 0.0f));
-        r.draw();
+        cabbage::SpriteRenderer        r;
+        std::vector<cabbage::Texture*> textures;
 
-        std::chrono::duration<float> elapsedTime = std::chrono::high_resolution_clock::now() - lastTime;
+        stbi_set_flip_vertically_on_load(true);
 
-        if (elapsedTime.count() >= 0.2f)
+        LOG(INFO) << "Init GLEW Complete";
+        cabbage::Texture* texture1 = cabbage::TextureLoader::load("resources/textures/arrowsRed.png");
+        textures.push_back(texture1);
+        cabbage::Texture* texture2 = cabbage::TextureLoader::load("resources/textures/arrowsGreen.png");
+        textures.push_back(texture2);
+
+        cabbage::Sprite      catSprite(textures[0]);
+        cabbage::Sprite      arrowsGreenSprite(textures[1]);
+        cabbage::SpriteSheet spriteSheet = cabbage::SpriteSheet(textures.at(0));
+        spriteSheet.addSpriteUVRect(0.0f, 0.66f, 0.33f, 0.33f);
+        spriteSheet.addSpriteUVRect(0.33f, 0.66f, 0.33f, 0.33f);
+        spriteSheet.addSpriteUVRect(0.66f, 0.66f, 0.33f, 0.33f);
+        spriteSheet.addSpriteUVRect(0.0f, 0.33f, 0.33f, 0.33f);
+        spriteSheet.addSpriteUVRect(0.33f, 0.33f, 0.33f, 0.33f);
+        spriteSheet.addSpriteUVRect(0.66f, 0.33f, 0.33f, 0.33f);
+        spriteSheet.addSpriteUVRect(0.0f, 0.0f, 0.33f, 0.33f);
+        spriteSheet.addSpriteUVRect(0.33f, 0.0f, 0.33f, 0.33f);
+        obj.SetSprite(&catSprite);
+        obj.GetSprite()->SetUVRect(spriteSheet.getSpriteUVRect(0));
+        child1.SetSprite(&arrowsGreenSprite);
+        child1.GetTransform().SetPosition(glm::vec3(0, 100, 0));
+        obj.addChild(&child1);
+        r.prepareDraw(&rootObj);
+
+        LOG(INFO) << "Init GLEW Complete";
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        const int                       target_fps = 60;
+        const std::chrono::milliseconds target_frame_duration(1000 / target_fps);
+        auto                            lastTime = std::chrono::high_resolution_clock::now();
+
+        int spriteId = 0;
+        while (!glfwWindowShouldClose(cwindow.GetGLFWwindow()))
         {
+            auto frame_start = std::chrono::high_resolution_clock::now();
+            glClear(GL_COLOR_BUFFER_BIT);
+            glPolygonMode(GL_FRONT, GL_FILL);
+            glPolygonMode(GL_BACK, GL_LINE);
+
+            obj.GetSprite()->SetUVRect(spriteSheet.getSpriteUVRect(spriteId));
+            obj.GetTransform().SetPosition(glm::vec3(100.0f * spriteId, 100.0f, 0.0f));
+            r.draw();
+
+            std::chrono::duration<float> elapsedTime = std::chrono::high_resolution_clock::now() - lastTime;
+
+            if (elapsedTime.count() >= 0.2f)
+            {
             spriteId++;
             if (spriteId > 7)
             {
-                spriteId = 0;
+            spriteId = 0;
             }
             lastTime = std::chrono::high_resolution_clock::now();
-        }
+            }
 
-        glfwSwapBuffers(cwindow.GetGLFWwindow());
-        glfwPollEvents();
-        auto frame_end = std::chrono::high_resolution_clock::now();
+            glfwSwapBuffers(cwindow.GetGLFWwindow());
+            glfwPollEvents();
+            auto frame_end = std::chrono::high_resolution_clock::now();
 
-        std::chrono::duration<double, std::milli> frame_time = frame_end - frame_start;
-        if (frame_time < target_frame_duration)
-        {
+            std::chrono::duration<double, std::milli> frame_time = frame_end - frame_start;
+            if (frame_time < target_frame_duration)
+            {
             std::this_thread::sleep_for(target_frame_duration - frame_time);
+            }
         }
     }
     glfwDestroyWindow(cwindow.GetGLFWwindow());
